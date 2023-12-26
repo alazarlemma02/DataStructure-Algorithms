af5cea8375f21a550fa7b5e93323e6b4500d25c3
alazarlemma02 <alazar03lemma@gmail.com>
2023-12-26 10:54:26 +0300

    added some practical questions

--- searching/BinarySearch.java
+++ searching/BinarySearch.java
@@ -1,21 +1,36 @@
 public class BinarySearch {
-    public static String binarySearch(int[] arr, int target){
+
+    /*
+     * Example-1
+     * Given a sorted array perform a binary search to find the index of '10'
+     * show each steps
+     * questions to answer:
+     * * Index
+     * * total number of operation taken
+     *
+     * a. [2, 4, 6, 8, 10, 12, 14, 16]
+     *
+     * b. [8, 10, 12, 14, 16, 2, 4, 6]
+     */
+    public static String binarySearch(int[] arr, int target) {
         // Binary search works only on sorted arrays
         int min = 0;
         int max = arr.length - 1;
-        while(min <= max){
+        while (min <= max) {
             int mid = (min + max) / 2;
-            if(arr[mid] == target){
+            if (arr[mid] == target) {
                 return String.format("Number %s found at index => %s", target, mid);
-            }
-            else if(arr[mid] < target){
+            } else if (arr[mid] < target) {
                 min = mid + 1;
-            }
-            else if(arr[mid] > target){
+            } else if (arr[mid] > target) {
                 max = mid - 1;
             }
         }
         return "Number not found!";
     }
 }
-// Exercise try to write the same method in a recursive way
+/* Home work
+
+Try to write the same method in a recursive way
+
+*/
