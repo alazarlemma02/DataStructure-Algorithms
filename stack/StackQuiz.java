4d0154b260910086e21ab3e45f41928321bdc694
alazarlemma02 <alazar03lemma@gmail.com>
2024-01-23 15:41:59 +0300

    quiz

---
+++ stack/StackQuiz.java
@@ -0,0 +1,15 @@
+public class StackQuiz {
+    /*
+     * Question 1:
+     *
+     *  => (A + B * C) / (D - E) * (F + G)
+     *
+     * a. find the postfix expressions. (show each steps)
+     *
+     * Question 2:
+     *
+     *  => ((A + B) * C) - (D / E + (F - G))
+     *
+     * a. find the postfix expressions.(show each steps)
+     */
+}
